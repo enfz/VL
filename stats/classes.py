83bb2e91ac9f2f99d86b5799c1682cd4a3fac32b
Enfei <ecjz01@users.noreply.github.com>
2022-05-12 17:10:44 -0400

    Started

---
+++ stats/classes.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+class Team:
+    region: str
+    players: list[Player]
+
+    def __init__(self, region: str, players: list):
+        self.region = region
+        self.players = players
+
+
+class Player:
+    name: str
+    org: str
+    acs: float
+    kd: float
+    kast: int
+    adr: float
+    kpr: float
+    apr: float
+    fkpr: float
+    fdpr: float
+    hs: int
+    cl: int
+
+    def __init__(self, name: str, org: str, acs: float, kd: float, 
+                 kast: int, adr: float, kpr: float, apr: float,
+                 fkpr: float, fdpr: float, hs: int, cl: int):
+        self.name, self.org, self.acs, self.kd, self.kast, self.adr, \
+            self.kpr, self.apr, self.fkpr, self.fdpr, self.hs, self.cl = \
+            name, org, acs, kd, kast, adr, kpr,apr, fkpr, fdpr, hs, cl
+        
--- stats/readdata.py
+++ stats/readdata.py
@@ -1,6 +1,27 @@
 import requests
 import csv
+import classes
 
-response = requests.get("https://vlrggapi.herokuapp.com/news")
-print(response.text)
+response = requests.get("https://vlrggapi.herokuapp.com/stats/eu")
+
+
+def response_cleaner() -> list[Player]:
+    removeList = ["{", "}", ":", ",", ":{", ":[{"]
+    data = []
+    tempList = response.text.split('"')
+    for item in tempList:
+        if item in removeList:
+            tempList.remove(item)
+
+    for i in range(len(tempList)):
+        if tempList[i] == "player":
+            data.append(Player(tempList[i + 1], tempList[i + 3], tempList[i + 5], tempList[i + 7],
+                               tempList[i + 9], tempList[i + 11], tempList[i + 13], tempList[i + 15],
+                               tempList[i + 17]))
+    return data
+
+
+#with open('rankings.csv') as file:
+
+#    writer = csv.writer(file, delimeter=',', quotechar='""')
 
