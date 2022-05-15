250813b1906c8d2e0cd5766e9675ab461f6b5a57
Enfei <ecjz01@users.noreply.github.com>
2022-05-15 19:18:58 -0400

    Fixed

--- stats/readdata.py
+++ stats/readdata.py
@@ -1,6 +1,6 @@
 import requests
 import csv
-
+from players import Player
 
 
 response = requests.get("https://vlrggapi.herokuapp.com/stats/eu")
@@ -10,15 +10,15 @@ def response_cleaner() -> list[Player]:
     remove_list = ["{", "}", ":", ",", ":{", ":[{"]
     data = []
     temp_list = response.text.split('"')
-    for item in templist:
+    for item in temp_list:
         if item in remove_list:
             temp_list.remove(item)
 
-    for i in range(len(tempList)):
+    for i in range(len(temp_list)):
         if temp_list[i] == "player":
-            data.append(Player(tempList[i + 1], tempList[i + 3], tempList[i + 5], tempList[i + 7],
-                               tempList[i + 9], tempList[i + 11], tempList[i + 13], tempList[i + 15],
-                               tempList[i + 17]))
+            data.append(Player(temp_list[i + 1], temp_list[i + 3], temp_list[i + 5], temp_list[i + 7],
+                               temp_list[i + 9], temp_list[i + 11], temp_list[i + 13], temp_list[i + 15],
+                               temp_list[i + 17]))
     return data
 
 
